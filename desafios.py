ca01842a149f0b50e8786d45442f2198492ef122
doginnn <diogenesemmanuel@gmail.com>
2021-01-05 20:53:57 -0300

    [UPD]Atualizando o desafio

--- desafios.py
+++ desafios.py
@@ -1,17 +1,17 @@
 # QUESTÃO 1
-# print("Código verifica se uma palavra começa com 'B' e termina com 'A'")
+print("Código verifica se uma palavra começa com 'B' e termina com 'A'")
 
-# inicio = "b"
-# fim = "a"
+inicio = "b"
+fim = "a"
 
-# nome = input("Digite um nome: ")
+nome = input("Digite um nome: ")
 
-# if nome[0] == inicio and nome[-1] == fim:
-#     print("A palavra ",nome, "termina com 'A' e começa com 'B', a palavra é valida. Parabéns!")
-# else:
-#     print("A palavra ",nome, "não atende aos requisitos, tente novamente!")
+if nome[0] == inicio and nome[-1] == fim:
+    print("A palavra ",nome, "termina com 'A' e começa com 'B', a palavra é valida. Parabéns!")
+else:
+    print("A palavra ",nome, "não atende aos requisitos, tente novamente!")
 
-
+#---------------------------------------------------------------
 # QUESTÃO 2
 '''
 Faça um programa que peça ao usuário para digitar um número inteiro,
@@ -32,3 +32,33 @@ if num1.isdigit():
 
 else:
     print(f"{num1} não é um número inteiro, Digite apenas números inteiros!")
+
+#---------------------------------------------------------------
+# QUESTÃO 3
+'''
+Faça um programa que tenha um input da hora e que baseando-se no input exiba
+a saudação apropriada. Ex: Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-24
+'''
+
+hora = input("Digite um horário: ")
+
+if hora.isdigit():
+    hora = int(hora)
+
+    if hora <= 11:
+        print(f"São {hora}hs, Bom Dia!")
+
+    elif hora >= 12 and hora <=17:
+        print(f"São {hora}hs, Boa Tarde!")
+
+    elif hora >= 18 and hora <= 24:
+        print(f"São {hora}hs, Boa Noite!")
+
+    else:
+        print("Não existe mais que 24hs no dia!")
+
+else:
+    print(f"'{hora}' Não é um horário válido, tente novamente!")
+
+#---------------------------------------------------------------
+#QUESTÃO 4
