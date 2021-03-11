1f23eb633afb025b7a60f59f83d3d29abe6f0f09
doginnn <diogenesemmanuel@gmail.com>
2021-03-10 21:22:00 -0300

    [UPD]Atualizando com Game de Perguntas e Respostas.

--- desafios.py
+++ desafios.py
@@ -317,5 +317,54 @@ print(comparador_fizzbuzz(int(input('Digite um número: '))))
 # ---------------------------------------------------------------
 # QUESTÃO 14
 '''
-Próxima Questão
+Game de Perguntas e Respostas
 '''
+print()
+print('Esse é um programa de perguntas e respostas.')
+print('Você deve escolher apenas uma alternativa por pergunta.')
+
+perguntas = {
+    'Pergunta "1"': {
+        'pergunta': 'Quanto é 2+2 ?',
+        'respostas': {
+            'a': '1',
+            'b': '4',
+            'c': '5',
+        },
+        'resposta_certa': 'b',
+    },
+    'Pergunta "2"': {
+        'pergunta': 'Quanto é 3*2 ?',
+        'respostas': {
+            'a': '4',
+            'b': '10',
+            'c': '6',
+        },
+        'resposta_certa': 'c',
+    },
+}
+print()
+
+respostas_certas = 0
+for pk, pv in perguntas.items():
+    print(f'{pk}: {pv["pergunta"]}')
+
+    print('Respostas: ')
+    for rk, rv in pv['respostas'].items():
+        print(f'[{rk}]: {rv}')
+    
+    resposta_usuario = input('Sua resposta é? ')
+
+    if resposta_usuario == pv['resposta_certa']:
+        print('Parabéns, você acertou!!! :)')
+        respostas_certas += 1
+    else:
+        print('Que pena, você errou!!! ;(')
+
+    print()
+
+qtd_perguntas = len(perguntas)
+porcentagem_acerto = respostas_certas / qtd_perguntas * 100
+
+print(f'Você acertou {respostas_certas} respostas ')
+print(f'Sua porcentagem de acerto foi {porcentagem_acerto}% ')
