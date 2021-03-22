3972b1b7b90cb5e6b9a6c3edbbb4e40cddcf8b48
doginnn <diogenesemmanuel@gmail.com>
2021-03-22 20:06:20 -0300

    [UPD]Usando 'Try', 'Except' como condicional..

--- desafios.py
+++ desafios.py
@@ -526,8 +526,26 @@ for agrupamento, valores_agrupados in alunos_agrupados:
 # ---------------------------------------------------------------
 # QUESTÃO 21
 """
-
+Usando 'Try', 'Except' como condicional.
 """
+def converte_numero(valor):
+    try:
+        valor = int(valor)
+        return valor
+    except ValueError:
+        try:
+            valor = float(valor)
+            return valor
+        except ValueError:
+            pass
+
+while True:
+    numero = converte_numero(input('Digite um número: '))
+
+    if numero is None:
+        print("Error: Thi's not a number.")
+    else:
+        print(numero * 10)
 
 # ---------------------------------------------------------------
 # QUESTÃO 22
